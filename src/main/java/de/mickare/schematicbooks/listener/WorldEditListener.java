45964a2d36975642f30daa97db090938cf4fc87f
mickare <kaeser.micha@googlemail.com>
2016-09-19 04:53:01 +0200

    worldedit clean fix

--- src/main/java/de/mickare/schematicbooks/listener/WorldEditListener.java
+++ src/main/java/de/mickare/schematicbooks/listener/WorldEditListener.java
@@ -17,9 +17,7 @@ public class WorldEditListener extends AbstractListener {
   public void onSetCommand(PlayerCommandPreprocessEvent event) {
     String msg = event.getMessage().toLowerCase();
     if (msg.startsWith("//set ")) {
-      if (msg.startsWith("0", 6) || msg.startsWith("air", 6)) {
-        CleanCommand.cleanSelection(event.getPlayer(), false, false);
-      }
+      CleanCommand.cleanSelection(event.getPlayer(), false, false);
     }
   }
 
